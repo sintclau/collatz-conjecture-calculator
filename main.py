fd2da70c50c17d5837c5609f4fb11544544afcc2
Claudiu Spiescu <claudiuspiescu@gmail.com>
2023-04-27 18:35:15 +0300

    Added a better way to see the conjecture

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 counter = 0
+nCounter = 0
 print("Please enter a natural number:")
 n = int(input())
 axisX = []
@@ -13,11 +14,12 @@ plt.title("Collatz Conjecture Graph")
 
 def main():
     global counter
+    global nCounter
     global n
     axisX.append(n)
     # f(x)=n/2 for n%2=0 and f(x)=3*n+1 for n%2!=0
     highestNumberReached = 0
-    while n > 1:
+    while nCounter < 5:
         if n % 2 != 0:
             n = (3 * n) + 1
             axisX.append(n)
@@ -28,6 +30,8 @@ def main():
         if n > highestNumberReached:
             highestNumberReached = n
         print(str(counter) + " -> " + str(n))
+        if n == 1:
+            nCounter += 1
     if n == 1:
         print("The total number of calculations: " + str(counter))
         print("The highest number reached is: " + str(highestNumberReached))
